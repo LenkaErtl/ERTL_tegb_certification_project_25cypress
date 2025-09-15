f4db2003a6362d5069ace54b793fa8a3b5f96f9a
Lenka Ertl <drahozalovalenka@seznam.cz>
2025-09-15 16:23:53 +0200

    přidán .gitattributes pro sjednocení konců řádků, přidána konfigurace TEGB URL a přihlašovací údaje

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,16 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    env: {
+      testVar: "Toto je proměnná",
+      username: "vikylucie@poesd.com",
+      password: "Test5577",
+      tegb_url: "https://tegb-frontend-88542200c6db.herokuapp.com/",
+      tegb_api_url: "https://tegb-backend-877a0b063d29.herokuapp.com/",
+    },
     watchForFileChanges: false,
     defaultCommandTimeout: 10000,
+    video: false,
+    chromeWebSecurity: false,
   },
 });
